17da2b075c50b12e89b5e1f1b945f14d8c048816
sprattlin <sprattl@g.cofc.edu>
2022-01-19 12:15:07 -0500

    homework 1 completed

--- assignments/hw1/hw1.py
+++ assignments/hw1/hw1.py
@@ -1,35 +1,49 @@
 """
-Name: <your name goes here – first and last>
+Name: <Lindsay Spratt>
 <ProgramName>.py
 
 Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
-
+This program solves math equations in everyday life and is able to interpret a wide variety of variables.
 Certification of Authenticity:
-<include one of the following>
-I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
+<I certify this assignment is my own work, but I discussed it with: Maeve O'toole>
 """
 
 
 def calc_rec_area():
-    pass
-
+    length = eval(input("Enter the length: "))
+    width = eval(input("Enter the width: "))
+    area = length * width
+    print("Area =", area)
 
 def calc_volume():
-    pass
-
+    length = eval(input("enter the Length: "))
+    width = eval(input("enter the width: "))
+    height = eval(input("enter the height: "))
+    volume = length * width * height
+    print("volume = ", volume)
 
 def shooting_percentage():
-    pass
-
+    shots = eval(input("enter total shots: "))
+    shots_made = eval(input("enter shots made: "))
+    shooting_percentage = (shots_made / shots) * 100
+    print("shooting_percentage = ", shooting_percentage)
 
 def coffee():
-    pass
+    pounds_coffee = eval(input("How many pounds of coffee would you like? : "))
+    cost = (0.86 * (10.50 * pounds_coffee)) + 1.5
+    print("Your total amount is $", cost)
 
 
 def kilometers_to_miles():
-    pass
+    kilometers = eval(input("How many kilometers did you drive?: "))
+    miles = (kilometers * 0.6214)
+    print("You travelled", miles, "miles")
 
+def main():
+    calc_rec_area()
+    calc_volume()
+    shooting_percentage()
+    coffee()
+    kilometers_to_miles()
+main()
 
-if __name__ == '__main__':
-    pass
