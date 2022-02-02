ac079f59f23fe4a9d97a3f6a6d36d42836a005fa
sprattlin <sprattl@g.cofc.edu>
2022-02-02 16:13:21 -0500

    lab 3 completed

--- assignments/hw3/hw3.py
+++ assignments/hw3/hw3.py
@@ -12,11 +12,22 @@ I certify that this assignment is my own work, but I discussed it with: <Name(s)
 
 
 def average():
-    pass
-
+    value_count = eval(input("How many grades will you enter: "))    # variable input
+    acc = 0
+    for i in range(value_count):
+        values = eval(input("Please enter your values: "))
+        for x in range(int(values)):         # second loop is not working corectly, check variables
+            acc = acc + (values + values)     # sum
+            grade_average = acc/value_count      # average
+    print("Your average is", grade_average)
 
 def tip_jar():
-    pass
+    tip_amount = eval(input("How much would you like to donate?: "))
+    acc = 0
+    for i in range(tip_amount, 5):
+       acc = sum((for i in range(tip_amount, 5)))
+
+
 
 
 def newton():
@@ -32,4 +43,4 @@ def pi():
 
 
 if __name__ == '__main__':
-    pass
+    average()
---
+++ labs/lab3/lab3.py
@@ -0,0 +1,28 @@
+"""
+Name: <Lindsay Spratt>
+<lab3>.py
+
+Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Creating code for the sum and average of daily traffic
+Certification of Authenticity:
+<I certify this assignment is my own work>
+"""
+def traffic():
+    roads_surveyed = eval(input("Please state how many roads were surveyed: "))
+    acc = 0
+    all_roads = 0 # accumulator for total num of vehicles, needs to be outside the loop to stop repeating
+    for x in range(1, roads_surveyed + 1): # starting at 1 to make sure '0' doesn't print as an option, adding+1 at the end to now lose an input
+        acc = acc + 1
+        print("Please enter how many days road", x, "was surveyed: ", end='')  # using print to add more than one message
+        days_surveyed = eval(input(" "))           # using eval(input now to convert the input to an int
+        total_cars = 0
+        for i in range(1, days_surveyed + 1):   # starting at 1 to make sure '0' doesn't print as an option, adding+1 at the end to now lose an input
+            print("\tHow many cars travelled on day",i,"?: ", end='') # using variable i to see the loop of roads, i is the variable for day number/count
+            cars_travelled = eval(input(" "))   # using eval to convert to int
+            total_cars = cars_travelled + total_cars # average for vehicles per day
+        all_roads = all_roads + total_cars # this accumulator will add together total vehicles travelled on all roads
+        print("Road",i, "average vehicles per day is: ", total_cars/days_surveyed) # average vehicles per day
+    print("Total number of vehicles on all roads is ", all_roads) # total number of vehicles on all roads
+    print("Average number of vehicles per road is ", all_roads/x) # average number of vehicles per road
+
+traffic()
