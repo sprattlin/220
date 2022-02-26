aa08003a9ac23bcd6b1f08a5a4a510bdad438158
sprattlin <sprattl@g.cofc.edu>
2022-02-25 21:18:35 -0500

    homework 6

--- assignments/hw6/hw6.py
+++ assignments/hw6/hw6.py
@@ -1,54 +1,73 @@
 """
 Name: <your name goes here – first and last>
-<ProgramName>.py
+<hw6>.py
 
 Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
-
+Writing code with formatting, parameters, arguments and creating ciphers
 Certification of Authenticity:
 <include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
 
-
+import math
 def cash_converter():
-    pass
-
+    cash = eval(input("Please enter an integer: "))
+    print("That is ${:.2f}".format(cash))
 
 def encode():
-    pass
-
+    message = input("Enter a message: ")
+    key = input("Enter a key: ")
+    for i in range(len(message)):
+        first_conv = ord(message[i])
+        x = first_conv + int(key)
+        convert_back = chr(x)
+        print(convert_back, end="")
 
 def sphere_area(radius):
-    pass
-
+    # radius = eval(input("Please enter the float value of the radius: "))
+    area = (4.0 * math.pi * (radius**2))
+    return area
 
 def sphere_volume(radius):
-    pass
-
+    # radius = eval(input("Please enter the float value of the radius: "))
+    volume = (4.0/3.0) * math.pi * (radius**3)
+    return volume
 
 def sum_n(number):
-    pass
-
+    # lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    result = sum(number)
+    return result
 
 def sum_n_cubes(number):
-    pass
+    # lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    squared = [number ** 2]
+    result = sum(squared)
+    return result
 
 
 def encode_better():
-    pass
-
+    message = input("Please enter a message: ")
+    key = input("Please enter a key: ")
+    empty_str = ''
+    for i in range(len(message)):
+        conv_mes = ord(message[i]) - 65
+        conv_key = ord(key[i % len(key)]) - 65
+        shift = (conv_mes + conv_key) % 58
+        cipher = shift + 65
+        cipher_conv = chr(cipher)
+        empty_str += cipher_conv
+    print(empty_str)
 
 if __name__ == '__main__':
-    # cash_converter()
-    # encode()
-    # res = sphere_area(13)
-    # print(res)
-    # res = sphere_volume(13)
-    # print(res)
-    # res = sum_n(100)
-    # print(res)
-    # res = sum_n_cubes(13)
-    # print(res)
-    # encode_better()
-    pass
+    cash_converter()
+    encode()
+    res = sphere_area(13)
+    print(res)
+    res = sphere_volume(13)
+    print(res)
+    res = sum_n(100)
+    print(res)
+    res = sum_n_cubes(13)
+    print(res)
+    encode_better()
+
---
+++ notes/data 101 notes.py
@@ -0,0 +1,6 @@
+def adjust_wages_by_rate(wages, rates):
+    for i in range(len(wages)):
+        var = wages[i] * rates
+wages = [20.00, 30.00, 15.00, 19.50, 13.00] 
+rate =  1.5
+print(wages)
