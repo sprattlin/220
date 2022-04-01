048f2d3fcc29f97a2bc87d756100048e1547b365
sprattlin <sprattl@g.cofc.edu>
2022-04-01 13:35:41 -0400

    homework 9

--- assignments/hw9/hw9.py
+++ assignments/hw9/hw9.py
@@ -1,23 +1,51 @@
+"""
+Name: <Lindsay Spratt>
+<hw9>.py
+
+Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Creating hangman game
+Certification of Authenticity:
+I certify that this assignment is entirely my own work.
+"""
+from random import randint
+
+
 def get_words(file_name):
-    pass
+    open_file = open(file_name, "r")
+    read_file = open_file.readlines()
+    return read_file
 
 
 def get_random_word(words):
-    pass
+    random_word = randint(words, len(get_words('words.txt')))  # int() argument must be a string, not list
+    return random_word
 
 
 def letter_in_secret_word(letter, secret_word):
-    pass
+    return letter in secret_word
 
 
 def already_guessed(letter, guesses):
-    pass
+    return letter in guesses
 
 
 def make_hidden_secret(secret_word, guesses):
-    pass
-
-
+    acc = ''
+    for characters in secret_word:
+        hidden = secret_word.replace(characters, ' _ ')
+        acc += hidden
+        if guesses in secret_word:
+            show_letter = secret_word.replace(acc, guesses)
+            show_letter.append(guesses)
+            return show_letter
+    #     acc += hidden
+    #     return acc
+    # if guesses in secret_word:
+    #     show_letter = secret_word.replace(acc, guesses)  # missing first and last letter, and space in between
+    #     return show_letter
+#
+# builtin_function_or_method object is not subscript able
+# not getting all the letters needed
 def won(guessed):
     pass
 
