c86820555ffd46e26c6ffbbf416b261be9e684fa
sprattlin <sprattl@g.cofc.edu>
2022-04-16 23:57:29 -0400

    homework 11

--- assignments/hw11/sales_force.py
+++ assignments/hw11/sales_force.py
@@ -3,31 +3,90 @@ Name: <Lindsay Spratt>
 <homework11>.py
 
 Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
-
+Creating classes for employee data
 Certification of Authenticity:
 <include one of the following>
-I certify that this assignment is entirely my own work.
+I certify that this assignment is entirely my own work, but I discussed it with Ashley Woods
 """
+
+from sales_person import SalesPerson
 
 
 class SalesForce:
-    def __init__(self, sales_people):
+    def __init__(self):
         self.sales_people = []
 
     def add_data(self, file_name):  # -> void
-        file_open = open('sales_data.txt', 'r')
+        file_open = open(file_name, 'r')
         file_read = file_open.read()
-        word_list = file_read.split()
-        self.sales_people += word_list
+        word_list = file_read.split('\n')
+        for i in range(len(word_list)):  # line is holding an integer
+            current_line = word_list[i]
+            current_line = current_line.split(', ')
+            id = current_line[0]
+            name = current_line[1]
+            # now make a salesperson object
+            person = SalesPerson(id, name)
+            sales = current_line[2].split()
+            for individual in sales:
+                sale = float(individual)
+                person.enter_sale(sale)
+            self.sales_people.append(person)
+        # each line of text in the file can become salesperson objects
 
     def quota_report(self, quota):  # -> list[list[int, string, float, bool]] -
-        pass
+        report = []
+        for person in self.sales_people:
+            person_list = []
+            check_id = person.get_id()
+            check_name = person.get_name()
+            check_sales = person.total_sales()
+            check_quota = person.met_quota(quota)
+            person_list.append(check_id)
+            person_list.append(check_name)
+            person_list.append(check_sales)
+            person_list.append(check_quota)
+            report.append(person_list)
+        return report
 
     def top_seller(self):  # list[SalesPerson]
-        pass
+        seller_list = []
+        top_sales = []
+        for sales in self.sales_people:
+            profit = sales.get_sales()
+            profit.sort()
+            highest_sale = profit[-1]
+            top_sales.append(highest_sale)
+            top_sales.sort()
+        if top_sales[-1] in top_sales[-2]:
+            for i in self.sales_people:
+                if top_sales[-1] in i.get_sales():
+                    seller_list.append(i)
+                    return seller_list
+        elif top_sales[-1] == top_sales[-2]:
+            for i in self.sales_people:
+                if top_sales[-1] in i.get_sales():
+                    seller_list.append(i)
+                if top_sales[-2] in i.get_sales():
+                    seller_list.append(i)
+                    return seller_list
 
-    def individual_sales(self, employee_id): # SalesPerson returns the SalesPerson & given employee_id or None if id n/a
-        pass
+    def individual_sales(self,
+                         employee_id):  # SalesPerson returns the SalesPerson & given employee_id or None if id n/a
+        for i in self.sales_people:
+            if employee_id == i.get_id():
+                return 1
 
-    def set_sale_frequencies(self):  # dict{sale amount: frequency} -
-        pass
+    def get_sale_frequencies(self):  # dict{sale amount: frequency} -
+        sales_list = []
+        for i in self.sales_people:
+            sales = i.get_sales()
+            sales_list.append(sales)
+        frequency = {}
+        for occurrence in sales_list:
+            for item in occurrence:
+                if item in frequency:
+                    frequency[item] += 1
+                else:
+                    frequency[item] = 1
+        return frequency
--- assignments/hw11/sales_person.py
+++ assignments/hw11/sales_person.py
@@ -3,10 +3,10 @@ Name: <Lindsay Spratt>
 <homework11>.py
 
 Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
-
+Creating classes for employee data
 Certification of Authenticity:
 <include one of the following>
-I certify that this assignment is entirely my own work.
+I certify that this assignment is entirely my own work, but I discussed it with Ashley Woods
 """
 
 
@@ -24,10 +24,9 @@ class SalesPerson:
 
     def set_name(self, name):  # void - sets the sales person’s name
         self.name = name
-        # probably wrong
 
-    def enter_sales(self, sale):  # void - adds the value of sale to the sales list
-        sale += self.sales
+    def enter_sale(self, sale):  # void - adds the value of sale to the sales list
+        self.sales.append(sale)
 
     def total_sales(self):   # returns the sum of the sales person’s sales
         return sum(self.sales)
@@ -36,20 +35,21 @@ class SalesPerson:
         return self.sales
 
     def met_quota(self, quota):  # boolean - returns True if the total sales meet or exceed the quota provided
-        if quota >= self.total_sales:
+        if quota >= self.total_sales():
             return True
+        else:
+            return False
 
     def compare_to(self, other):  # -> int
-        other = SalesPerson
-        if self.total_sales(other) > self.total_sales():
+        if other.total_sales() > self.total_sales():
             return -1
-        if self.total_sales(other) < self.total_sales():
+        if other.total_sales() < self.total_sales():
             return 1
         else:
             return 0
 
-    def main(self):
-        pass
+    def __str__(self):  # same format as constructor  returns “<employee_id>-<name>: <total sales>”
+        return str(self.get_id()) + "-" + self.get_name() + ': ' + str(self.total_sales())
 
 
 
